2ccb0b56a118a45fa013e0fe5aa3ddfe61d85ec6
Simion <simion.okola@student.moringaschool.com>
2022-09-08 11:52:20 +0300

    Display Covid status for each country

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    
 let LastUpdate = document.getElementById('overgrade')
 let state = document.getElementById('state')
 let death = document.getElementById('dist')
-let high = document.getElementById('high')
-let low = document.getElementById('low')
+let recoveries = document.getElementById('high')
+let latitude = document.getElementById('low')
+let long = document.getElementById('long')
 
 fetch('https://raw.githubusercontent.com/zmsp/coronavirus-json-api/master/latest.json')
 .then(res =>res.json())
@@ -28,10 +30,10 @@ function display(array){
        but.addEventListener("click", () => {
         LastUpdate.textContent = `Last Updated: ${stud['Last Update']}`;
         state.textContent = `Confirmed Cases of Covid: ${stud['Confirmed']}`;
-        death.textContent = `Number of deaths: ${stud.Deaths}`;
-        state.innerContent = 
-        high.src = `Age: ${stud.age}`;
-        low.textContent = `Score: ${stud.score}`
+        death.textContent = `Number of Deaths: ${stud.Deaths}`;
+        recoveries.textContent = `Number of Recoveries: ${stud.Recovered}`;
+        latitude.textContent = `Latitude: ${stud.Latitude}`
+        long.textContent = `Longitude: ${stud.Longitude}`
        })
     }
 }
@@ -42,18 +44,29 @@ function display(array){
         let numConfirmed = []
         let numRecovered = []
         let totalDeath = 0;
+        let totalConfirmed = 0;
+        let totalRecovered = 0;
         for(let item of array){
-         numDeath.push(item.Deaths)
+        numDeath.push(item.Deaths)
+        numConfirmed.push[item.Confirmed]
+        numRecovered.push[item.Recovered]
         totalDeath = parseInt(totalDeath) + parseInt(item.Deaths)
+        totalConfirmed = parseInt(totalConfirmed) + parseInt(item.Confirmed)
+        totalRecovered = parseInt(totalRecovered) + parseInt(item.Recovered)
      }
         let avScore =  totalDeath/numDeath.length
-        console.log(avScore)
- 
+        let confirmed =  totalConfirmed/totalConfirmed.length
+        let recovered =  totalRecovered/totalRecovered.length
+
         let meanbutt = document.getElementById('meanbuton')
         meanbutt.addEventListener('click', ()=>{
          let meanVal = document.getElementById('mean')
-        meanVal.innerHTML = `The mean score for the class is ${avScore.toFixed(4)}`
+         let confirm = document.getElementById('mean1')
+         let recover = document.getElementById('mean2')
 
+        meanVal.innerHTML = `Global mean number of deaths: ${avScore.toFixed(4)}`
+        confirm.innerHTML = `Global mean number of Confirmed Cases: ${confirmed.toFixed(4)}`
+        recover.innerHTML = `Global mean number of recoveries: ${recovered.toFixed(4)}`
 
         let medianout = document.getElementById('median')
     
